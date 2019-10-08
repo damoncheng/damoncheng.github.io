57896fec9736b0a11bad31d770109e818c979671
damoncheng <354092565@qq.com>
2019-10-08 14:58:05 +0800

    adjust

---
+++ src/layouts/HomeLayout.vue
@@ -0,0 +1,50 @@
+<template>
+
+    <div> 
+      
+      <span>Hello Quasar</span>
+      <span class="el-icon-chenggong"></span>
+      <span class="el-icon-success"></span>
+
+    </div>
+
+</template>
+
+<script>
+
+/*
+import sqlite3 from 'sqlite3';
+
+var db = new sqlite3.Database(':memory:');
+
+db.serialize(function() {
+  db.run("CREATE TABLE lorem (info TEXT)");
+
+  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+
+  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+      console.log(row.id + ": " + row.info);
+  });
+});
+
+db.close();
+*/
+
+export default {
+  name: 'HomeLayout',
+  data () {
+    return {
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+</style>
